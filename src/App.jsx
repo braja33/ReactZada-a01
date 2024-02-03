174325b1a80e1f4a7834183e02860d3b58f5f179
Marko Bratković <marko.bratkovic01@gmail.com>
2024-02-03 11:32:51 +0100

    inicijalizacija Zadaća01 - JSX komponenta

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const App = () => {
+  let proizvod = {
+    ime: "mlijeko",
+    količina: 4,
+    pakiranje: "1L",
+    rokTrajanja: "10 dana",
+  };
+  return (
+    <>
+      <h1>Informacije o proizvodu {proizvod.ime}</h1>
+      <p>Količina {proizvod.količina}</p>
+      <p>
+        Pakiranje proizvoda {proizvod.ime} je {proizvod.pakiranje}
+      </p>
+      <p>
+        Rok trajanja za {proizvod.ime} je {proizvod.rokTrajanja}
+      </p>
+    </>
+  );
+};
+
+export default App;
